b7f566d5bc3726a0a45b8fd1adba9e608c106328
patturaja501 <patturaja501@gmail.com>
2026-04-02 08:38:28 +0530

    first commit

---
+++ server/configs/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+const connectDB=async()=>{
+    try {
+        mongoose.connection.on('connected',()=>console.log('DataBase Connected'))
+        await mongoose.connect(`${process.env.MONGODB_URL}/quickShow`)
+    } catch (error) {
+        console.log(error)
+    }
+}
+export default connectDB;
---
+++ server/inngest/index.js
@@ -0,0 +1,58 @@
+import { Inngest } from "inngest";
+import userModel from "../models/userModel.js";
+
+export const inngest = new Inngest({ id: "movie-ticket-booking" });
+
+// Inngest Function to Create user Data in dateBase
+const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+    triggers: [{ event: "clerk/user.created" }],
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await userModel.create(userData);
+  },
+);
+
+// Inngest Function to Deleted user Data in dateBase
+
+const syncUserDeletion = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
+  async ({ event }) => {
+    const { id } = event.data;
+    await userModel.findByIdAndDelete(id);
+  },
+);
+
+// Inngest Function to Update user Data in dateBase
+
+const syncUserUpdation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+    triggers: [{ event: "clerk/user.updated" }],
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await userModel.findByIdAndUpdate(id, userData);
+  },
+);
+
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
